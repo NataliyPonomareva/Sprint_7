60eda2fc852014b9eaa618000689f44a14e30771
Natasha Ponomareva <nataliyp0nomareva@yandex.com>
2024-09-09 13:56:55 +0300

    В фикстуре заменила обращение к ручке из констант.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@ import allure
 import pytest
 import requests
 
+from url_api import Api
+
 
 # Фикстура для удаления курьера"
 @allure.step('Удаление созданного курьера')
@@ -14,7 +16,7 @@ def cleanup_courier():
     # После завершения теста, если был создан курьер, удалим его
     if 'courier_id' in container:
         courier_id = container['courier_id']
-        response = requests.delete(f'https://qa-scooter.praktikum-services.ru/api/v1/courier/{courier_id}',
+        response = requests.delete(f'{Api.BASE_URL + Api.CREATE_COURIER}/{courier_id}',
                                    json={'id': courier_id})
         assert response.status_code == 200, f"Не удалось удалить курьера с идентификатором {courier_id}"
 
@@ -27,4 +29,4 @@ def created_orders():
 
     # После выполнения тестов удаляем созданные заказы
     for track in container_orders:
-        requests.delete(f'https://qa-scooter.praktikum-services.ru/api/v1/orders/{track}')
+        requests.delete(f'{Api.BASE_URL + Api.CREATE_ORDER}/{track}')
